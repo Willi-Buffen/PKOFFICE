846e57563ce5f8a64978c3ff9acd633041f4a0ad
willi willison wilkanson wallah billah buffen <buffen.willi@gmail.com>
2024-09-19 17:48:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Smooth scroll to section
+function scrollToSection(id) {
+    document.querySelector(id).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+// Form submission (dummy for now)
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+});
 
